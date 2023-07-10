fd830703a67875e395fa77acacc107254e9beda7
kokakokabot <kokicd1@gmail.com>
2023-07-10 15:01:00 +0200

    Ran out of time will continue tomorrow

--- pacman.py
+++ pacman.py
@@ -11,22 +11,31 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
 fps = 60
 font = pygame.font.Font('freesansbold.ttf', 20)
+level = boards
+color = 'blue'
 
-
-def draw_board(lvl):
+def draw_board():
     num1 = ((HEIGHT - 50) // 32)
     num2 = (WIDTH // 30)
-    for i in range(len(lvl)):
-        for j in range(len(lvl[i])):
-
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            if level[i][j] == 1:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5*num2), i * num1 + (0.5*num1)), 4)
+            if level[i][j] == 2:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5*num2), i * num1 + (0.5*num1)), 10)
+            if level[i][j] == 3:
+                pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
+                                 (j * num2 + (0.5 * num2), i * num1 + num1), 3)
+            if level[i][j] == 4:
+                pygame.draw.line(screen, color, (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
 
 
 run = True
-
 while run:
     timer.tick(fps)
     screen.fill('black')
-    draw_board(level)
+    draw_board()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
