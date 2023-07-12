4c814fe89641ee864bc0879a40fd9894a8a0aeb8
kokakokabot <kokicd1@gmail.com>
2023-07-12 08:46:13 +0200

    Added player and ghost images, animated pacman to work

--- pacman.py
+++ pacman.py
@@ -13,7 +13,15 @@ font = pygame.font.Font('freesansbold.ttf', 20)
 level = boards
 color = 'blue'
 PI = math.pi
+player_images = []
+direction = 0
+counter = 0
 
+for i in range(1, 5):
+    player_images.append(pygame.transform.scale(pygame.image.load(f'images/player/{i}.png'), (45, 45)))
+
+player_x = 450
+player_y = 663
 
 def draw_board():
     num1 = ((HEIGHT - 50) // 32)
@@ -47,14 +55,28 @@ def draw_board():
                                  (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
 
 def draw_player():
-    pass
+    # Directions are 0-right, 1-left, 2-up, 3-down
+    if direction == 0:
+        screen.blit(player_images[counter // 5], (player_x, player_y))
+    if direction == 1:
+        screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (player_x, player_y))
+    if direction == 2:
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (player_x, player_y))
+    if direction == 3:
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_x, player_y))
 
 
 run = True
 while run:
     timer.tick(fps)
+    if counter < 19:
+        counter += 1
+    else:
+        counter = 0
+
     screen.fill('black')
     draw_board()
+    draw_player()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
