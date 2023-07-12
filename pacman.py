f6fd9db400e1980dd431ec482646ddd8ef295bae
kokakokabot <kokicd1@gmail.com>
2023-07-12 15:38:35 +0200

    Added directional commands using arrow keys

--- pacman.py
+++ pacman.py
@@ -16,6 +16,7 @@ PI = math.pi
 player_images = []
 direction = 0
 counter = 0
+flicker = False
 
 for i in range(1, 5):
     player_images.append(pygame.transform.scale(pygame.image.load(f'images/player/{i}.png'), (45, 45)))
@@ -30,7 +31,7 @@ def draw_board():
         for j in range(len(level[i])):
             if level[i][j] == 1:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
-            if level[i][j] == 2:
+            if level[i][j] == 2 and not flicker:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
             if level[i][j] == 3:
                 pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
@@ -71,8 +72,11 @@ while run:
     timer.tick(fps)
     if counter < 19:
         counter += 1
+        if counter > 3:
+            flicker = False
     else:
         counter = 0
+        flicker = True
 
     screen.fill('black')
     draw_board()
@@ -81,6 +85,15 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                direction = 0
+            if event.key == pygame.K_LEFT:
+                direction = 1
+            if event.key == pygame.K_UP:
+                direction = 2
+            if event.key == pygame.K_DOWN:
+                direction = 3
 
     pygame.display.flip()
 
